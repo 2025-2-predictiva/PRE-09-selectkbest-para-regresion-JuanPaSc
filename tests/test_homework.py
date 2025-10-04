5579b37ac8aff45d5a5cc6d560d7f30a4d490902
Juan Pablo Salas Clavijo <jsalasc@unal.edu.co>
2025-10-04 09:23:20 -0500

    intento (espero que le definitivo)

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,7 +5,7 @@ def load_data():
 
     import pandas as pd
 
-    dataset = pd.read_csv("../files/input/auto_mpg.csv")
+    dataset = pd.read_csv("files/input/auto_mpg.csv")
     dataset = dataset.dropna()
     dataset["Origin"] = dataset["Origin"].map(
         {1: "USA", 2: "Europe", 3: "Japan"},
@@ -21,7 +21,7 @@ def load_estimator():
     import os
     import pickle
 
-    if not os.path.exists("../homework/estimator.pickle"):
+    if not os.path.exists("homework/estimator.pickle"):
         return None
     with open("homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
